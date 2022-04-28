bf5c8ea94243b4db2e0c17136c65eeafe3862955
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-04-28 18:10:45 -0500

    Add basic tailwind styles and component structure

---
+++ client/src/components/EntryItem/EntryItem.js
@@ -0,0 +1,29 @@
+export default function EntryItem({ entry, entryNumber, ...props }) {
+  const { title, time: listenDate, subtitle, artists } = entry;
+  return (
+    <div
+      className="flex items-stretch w-full bg-gray-200 rounded-2xl transition-colors
+      hover:bg-gray-300 
+      "
+    >
+      <div className={`bg-gray-400 w-[90px]`}>
+        <span className="flex justify-start items-center py-1 pl-4 text-xs font-bold text-white bg-gray-500">
+          #{entryNumber}
+        </span>
+      </div>
+
+      <div className="flex flex-col items-start p-4">
+        <span className="block text-base text-black">{title}</span>
+        <span className="block text-xs font-bold text-black">
+          {artists[0].name}
+        </span>
+        <p className="text-xs font-medium text-gray-700">
+          Listened at:{" "}
+          <span className="text-xs font-normal text-gray-400">
+            {listenDate}
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
