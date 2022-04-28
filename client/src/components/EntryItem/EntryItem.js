cb947c5cdca8d21e0c14aa0f67e690287cda739d
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-04-28 18:24:32 -0500

    Add function to get artist iniitials

--- client/src/components/EntryItem/EntryItem.js
+++ client/src/components/EntryItem/EntryItem.js
@@ -1,15 +1,17 @@
+function getInitialsFromArtist(artist) {
+  return artist.split(" ").reduce((prev, curr) => prev + curr[0], "");
+}
 export default function EntryItem({ entry, entryNumber, ...props }) {
   const { title, time: listenDate, subtitle, artists } = entry;
   return (
-    <div
-      className="flex items-stretch w-full bg-gray-200 rounded-2xl transition-colors
-      hover:bg-gray-300 
-      "
-    >
-      <div className={`bg-gray-400 w-[90px]`}>
-        <span className="flex justify-start items-center py-1 pl-4 text-xs font-bold text-white bg-gray-500">
+    <div className="flex items-stretch w-full bg-gray-200 rounded-2xl transition-colors hover:bg-gray-300">
+      <div
+        className={`bg-gray-400 w-[90px] relative flex items-center justify-center`}
+      >
+        <span className="flex absolute top-0 justify-start items-center py-1 pl-4 w-full text-xs font-bold text-white bg-gray-500">
           #{entryNumber}
         </span>
+        <span>{getInitialsFromArtist(artists[0].name)}</span>
       </div>
 
       <div className="flex flex-col items-start p-4">
