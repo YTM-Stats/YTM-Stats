bd1da9bb7e479e473aeda10f7c8c1be47ca29e43
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-04-28 11:24:21 -0500

    Format code

--- client/src/components/WatchHistoryDropZone/WatchHistoryDropZone.js
+++ client/src/components/WatchHistoryDropZone/WatchHistoryDropZone.js
@@ -3,34 +3,39 @@ import Button from "../Button/Button.js";
 import { useDropzone } from "react-dropzone";
 
 function fileNameValidator(file) {
-    if (file.name.toLowerCase() !== "watch-history.json" && file.name.toLowerCase() !== "watch-history") {
-        return {
-            code: "invalid file name",
-            message: "Expected a watch-history.json file"
-        }
-    }
-    return null
-
+  if (
+    file.name.toLowerCase() !== "watch-history.json" &&
+    file.name.toLowerCase() !== "watch-history"
+  ) {
+    return {
+      code: "invalid file name",
+      message: "Expected a watch-history.json file",
+    };
+  }
+  return null;
 }
 
-export default function WatchHistoryDropZone({className, onDrop, onDropAccepted, onDropRejected, ...props}) {
-
-  const {
-    getRootProps,
-    getInputProps,
-    isDragActive,
-    open,
-  } = useDropzone({
-      onDrop,
-      onDropAccepted,
-      onDropRejected,
+export default function WatchHistoryDropZone({
+  className,
+  onDrop,
+  onDropAccepted,
+  onDropRejected,
+  ...props
+}) {
+  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+    onDrop,
+    onDropAccepted,
+    onDropRejected,
     noClick: true,
     accept: ".json, application/json",
-      validator: fileNameValidator
+    validator: fileNameValidator,
   });
 
   return (
-    <label {...getRootProps()} className={`${className} block bg-gray-200 p-4 rounded-2xl w-full`}>
+    <label
+      {...getRootProps()}
+      className={`${className} block bg-gray-200 p-4 rounded-2xl w-full`}
+    >
       <div
         className={` px-8 py-20 bg-gray-200 border-4 rounded-2xl
       text-lg font-bold text-gray-500
