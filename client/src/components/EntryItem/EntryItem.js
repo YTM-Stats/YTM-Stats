15fab0540822e9889e84a3fec5eea39c0530f0a6
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-04-29 12:13:34 -0500

    Add support of artist entry type

--- client/src/components/EntryItem/EntryItem.js
+++ client/src/components/EntryItem/EntryItem.js
@@ -1,11 +1,31 @@
 function getInitialsFromArtist(artist) {
   return artist.split(" ").reduce((prev, curr) => prev + curr[0], "");
 }
-export default function EntryItem({ entry, entryNumber, ...props }) {
-  const { title, time: listenDate, artists } = entry;
+export default function EntryItem({
+  entry,
+  entryNumber,
+  type = "song",
+  ...props
+}) {
+  const artist =
+    type.toLowerCase() === "artist" ? entry.title : entry.artists[0].name;
+  let content = (
+    <>
+      {type !== "artist" && (
+        <span className="block text-xs font-bold text-black">{artist}</span>
+      )}
+      <p className="text-xs font-medium text-gray-700">
+        {type === "artist" ? "Songs played: " : "Listened at: "}
+        <span className="text-xs font-normal text-gray-400">
+          {type === "artist" ? `${entry.plays} times.` : entry.time}
+        </span>
+      </p>
+    </>
+  );
+
   return (
     <div
-      className="flex items-stretch w-full bg-gray-200 rounded-2xl transition-colors hover:bg-gray-300 overflow-hidden"
+      className="flex overflow-hidden items-stretch w-full bg-gray-200 rounded-2xl transition-colors hover:bg-gray-300"
       {...props}
     >
       <div
@@ -14,20 +34,12 @@ export default function EntryItem({ entry, entryNumber, ...props }) {
         <span className="flex absolute top-0 justify-start items-center py-1 pl-4 w-full text-xs font-bold text-white bg-gray-500">
           #{entryNumber}
         </span>
-        <span>{getInitialsFromArtist(artists[0].name)}</span>
+        <span>{getInitialsFromArtist(artist)}</span>
       </div>
 
       <div className="flex flex-col items-start p-4">
-        <span className="block text-base text-black">{title}</span>
-        <span className="block text-xs font-bold text-black">
-          {artists[0].name}
-        </span>
-        <p className="text-xs font-medium text-gray-700">
-          Listened at:{" "}
-          <span className="text-xs font-normal text-gray-400">
-            {listenDate}
-          </span>
-        </p>
+        <span className="block text-base text-black">{entry.title}</span>
+        {content}
       </div>
     </div>
   );
